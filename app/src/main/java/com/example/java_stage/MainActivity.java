512c431dc627bc55a5e4e102553f3827b602a0f6
vetukuri <varmavetukuri1234@gmail.com>
2024-01-19 19:33:33 +0530

    check

--- app/src/main/java/com/example/java_stage/MainActivity.java
+++ app/src/main/java/com/example/java_stage/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.java_stage.Myplugin;
 public class MainActivity extends AppCompatActivity {
 
     @Override
